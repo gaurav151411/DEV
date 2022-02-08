70c53bba6dab2f678553619aaff71399191a27f9
gaurav151411 <gauravsain15rrr@gmail.com>
2022-02-08 22:46:03 +0530

    fundamentals

---
+++ Javascript/index.js
@@ -0,0 +1,12 @@
+var  a;
+
+a=10;
+console.log(a);
+
+// a="hello";
+// console.log(a);
+// re -initialisation of variable a
+
+//typeof operator -->tells the type of data stored in a variable
+
+console.log(typeof a);
