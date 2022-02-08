cb1c08b90aafbe0333bf29cd87bad51225becda7
gaurav151411 <gauravsain15rrr@gmail.com>
2022-02-08 23:28:08 +0530

    new

--- Javascript/index.js
+++ Javascript/index.js
@@ -10,3 +10,42 @@ console.log(a);
 //typeof operator -->tells the type of data stored in a variable
 
 console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+// null
+a=null;
+console.log(a);
+console.log(typeof a); 
+
+// string 
+var b="hi";
+console.log(b);
+console.log(typeof b);
+
+var b='hi';
+console.log(b);
+console.log(typeof b);
+
+var b=`hi`;
+console.log(b);
+console.log(typeof b);
+
+var str;
+str="i am the man with the plan";
+console.log(str);
+
+var str;
+str="i am the man with the \nplan";
+console.log(str);
+
+var str;
+str=`i am the man with the 
+plan`;
+console.log(str);
+
+var num=150;
+str=`half of ${num} is ${num/2}`;
+console.log(str);
