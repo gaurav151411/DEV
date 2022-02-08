547d26c4c363f21d198e3d743e01a1724e00b83c
gaurav151411 <gauravsain15rrr@gmail.com>
2022-02-08 22:57:14 +0530

    js started

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,8 +3,8 @@ var  a;
 a=10;
 console.log(a);
 
-// a="hello";
-// console.log(a);
+a="hello";
+console.log(a);
 // re -initialisation of variable a
 
 //typeof operator -->tells the type of data stored in a variable
