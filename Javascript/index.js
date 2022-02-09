a6c158870e73ef046cca149925939b26e910ea18
gaurav151411 <gauravsain15rrr@gmail.com>
2022-02-10 00:08:23 +0530

    updated

--- Javascript/index.js
+++ Javascript/index.js
@@ -21,6 +21,8 @@ console.log(a);
 console.log(typeof a); 
 
 // string 
+// using " ",' ',` `,
+// " " and '' have same use but ` ` is used dynamically
 var b="hi";
 console.log(b);
 console.log(typeof b);
@@ -49,3 +51,35 @@ console.log(str);
 var num=150;
 str=`half of ${num} is ${num/2}`;
 console.log(str);
+
+// var has some problems
+// 1. redeclaration is allowed
+
+var num=300;
+console.log(num);
+
+var num=30;
+console.log(num);
+
+// to overcome this problem
+// let keyword is used
+// let num=100;
+// console.log(num);
+
+// let num=50;
+// console.log(num);
+// error will be there
+// therefore,we can't re-declare a variable using let, but not re-initialise it
+
+// loops in js
+var num=10;
+for(var i=1;i<=num;i++){
+    if(i%2==0)
+       console.log("even");
+    else
+       console.log("odd");    
+} 
+
+const n1=2;
+console.log(n1);
+// neither re-declaration nor re-initialisation
